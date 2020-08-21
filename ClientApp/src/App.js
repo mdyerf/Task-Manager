33493e9bc0cca7d1f65a48965b30a5798298aae5
mdyerf <mdyerf1380@gmail.com>
2020-08-21 11:55:29 +0430

    first version

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Task from './components/Task';
+
+import './components/style.css';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -10,8 +12,16 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/task' component={Task} />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/task' component={Task} />
+          <Route component={() => 
+            <h1 className="text" style={{marginTop: '50%'}}>
+            404 - یافت نشد :(
+            </h1>
+            }
+          />
+        </Switch>
       </Layout>
     );
   }
--- ClientApp/src/components/DownloadApp.js
+++ ClientApp/src/components/DownloadApp.js
@@ -8,9 +8,8 @@ function DownloadApp() {
             <p className="text" >اطلاعاتی از سیستم شما در دیتابیس موجود نیست</p>
             <p className="text" >برای ارسال اطلاعات سیستم برنامه ی زیر را دانلود و اجرا کنید</p>
 
-            <a href={"http://s000.tinyupload.com/?file_id=80714589853407229486"} 
-                rel="noreferrer" target="_blank"
-            >
+            <a href=' http://s000.tinyupload.com/?file_id=65505557202777597474'
+               target='_blank' >
                 دریافت برنامه
             </a>
             <p className="text">پس از اجرای برنامه صفحه را refresh کنید</p>
